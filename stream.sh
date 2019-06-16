6d9b9e6cf8207eb29fcc2eccd0bcf3b64ca357a4
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2019-06-16 16:21:22 +0100

    wip
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- stream.sh
+++ stream.sh
@@ -18,7 +18,8 @@ bitrate="${AV_VIDEO_BITRATE:-2000000}"
 raspivid_options="-vf"
 
 # Do it
-raspivid -o - -t 0 -w $width -h $height -ev 25 -ex nightpreview -fps $fps -b $bitrate $raspivid_options | \
-tee /data/video_$(date +%F_%T).h264 | \
-ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -f h264 -i - -vcodec copy -c:a aac -ar 44100 -b:a 128k -f flv $stream/$key
+#raspivid -o - -t 0 -w $width -h $height -ev 25 -ex nightpreview -fps $fps -b $bitrate $raspivid_options | \
+#tee /data/video_$(date +%F_%T).h264 | \
+#ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -f h264 -i - -vcodec copy -c:a aac -ar 44100 -b:a 128k -f flv $stream/$key
 
+raspivid -t 0 -w $width -h $height -ev 25 -ex nightpreview -fps $fps -b $bitrate $raspivid_options -o /data/video_$(date +%F_%T).h264
